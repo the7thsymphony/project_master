0e308f80c0b195075644c5f9ca7d87be4cc08a27
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-18 15:14:21 +0100

    messsage controller done

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,23 +1,56 @@
 class MessageController < ApplicationController
 
+before_action :find_message, only: [:show, :destroy]
+
   def index
+
+    @messages = Message.all
+
   end
 
   def new
 
+    @message = Message.new
+
+  end
+
+  def show
+    @message = Message.new
   end
 
   def create
+
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to @message
+    else
+      render :new
+
+  end
 
   end
 
   def edit
 
-  end
-
-  def delete
+     @message = Message.find(params[:id])
 
   end
 
+  def destroy
+    @message.destroy
+    redirect_to message_path
+
+  end
+
+  private
+
+  def find_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+
+  end
 
 end
