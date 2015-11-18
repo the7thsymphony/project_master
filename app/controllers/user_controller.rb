80738671c6cd1595f0903910c012f39214417ac7
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-18 12:05:59 +0100

    change routes.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,34 @@
 class UserController < ApplicationController
+
+
+  def index
+    @users = User.all
+
+  end
+
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[])
   end
 
   def show
+
   end
 
   def edit
+
   end
 
   def delete
+
   end
+
+
+
+  @user = User.create(params[])
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :projects , only: [:new, :create,:show, :edit, :delete, :index] do
-    resources :applicants , only: [:show, :delete, :index]
+    resources :projects_jobs , only: [:show, :delete, :index] do
+      resources :applicants , only: [:show, :delete, :index]
+    end
   end
 
   resources :messages, only: [:new, :create, :edit, :delete, :index]
 
-  resources :users , only: [:new, :create,:show, :edit, :delete]
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 end
